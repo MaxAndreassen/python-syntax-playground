0b2fda7b5beed2bc9b978179584da3d4dd33f826
Max Andreassen <andreassenmax@gmail.com>
2021-07-17 18:16:34 +0100

    extract fib methods into helper file

--- game.py
+++ game.py
@@ -1,22 +1,3 @@
-def fib(n):
-    if (n == 1 or n == 2):
-        result = 1;
-    else:
-        result = fib(n - 1) + fib(n - 2);
-    return result;
-
-def fib_memo(n, memo):
-    if (memo == None):
-        memo = [None] * (n + 1);
-
-    if (memo[n] != None):
-        return memo[n];
-
-    if (n == 1 or n == 2):
-        result = 1;
-    else:
-        result = fib_memo(n - 1, memo) + fib_memo(n - 2, memo);
-    memo[n] = result;
-    return result;
+from helpers.fib import fib_memo;
 
 print(fib_memo(100, None));
---
+++ helpers/fib.py
@@ -0,0 +1,20 @@
+def fib(n):
+    if (n == 1 or n == 2):
+        result = 1;
+    else:
+        result = fib(n - 1) + fib(n - 2);
+    return result;
+
+def fib_memo(n, memo):
+    if (memo == None):
+        memo = [None] * (n + 1);
+
+    if (memo[n] != None):
+        return memo[n];
+
+    if (n == 1 or n == 2):
+        result = 1;
+    else:
+        result = fib_memo(n - 1, memo) + fib_memo(n - 2, memo);
+    memo[n] = result;
+    return result;
